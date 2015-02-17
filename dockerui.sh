605b2992e29a071f24caedb63f1b3a55fcd8f652
Robert Jacob <robert.jacob@holidaycheck.com>
2015-02-17 17:19:59 +0100

    Add wrapper script for dockerui.

---
+++ dockerui.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# dockerui is a simple web UI for controlling the docker daemon
+#
+# This is a simple helper script to pull the image and start a container
+# accessing the local docker daemon (via UNIX socker)
+#
+# Expects port tcp/9000 to be unused.
+#
+docker inspect dockerui | grep -q "\"Running\": true" && {
+  echo Stopping running container...
+  docker stop dockerui
+}
+
+echo Checking for new version...
+docker pull dockerui/dockerui | grep -q "Downloaded newer image" && {
+  echo New image version. Removing container...
+  docker rm dockerui
+}
+
+echo Starting dockerui...
+docker start dockerui || docker run --name dockerui -d -v /var/run/docker.sock:/var/run/docker.sock -p 9000:9000 dockerui/dockerui
