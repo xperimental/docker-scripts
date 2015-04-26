1d89bdda21d691756bc385ccf9c268755201e753
Robert Jacob <xperimental@solidproject.de>
2015-04-26 13:32:07 +0200

    Clean volumes from removed containers.

--- docker-clean-exited.sh
+++ docker-clean-exited.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Cleans the docker daemon of all containers which are not running anymore
-docker ps -aqf status=exited | xargs docker rm
+docker ps -aqf status=exited | xargs docker rm -v
