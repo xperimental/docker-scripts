1bcad7d353b2759a4e82882d5903b50f8a28febe
Robert Jacob <robert.jacob@holidaycheck.com>
2016-05-10 12:12:31 +0200

    Use new dockerui image.

--- dockerui.sh
+++ dockerui.sh
@@ -6,16 +6,19 @@
 #
 # Expects port tcp/9000 to be unused.
 #
+
+readonly IMAGE=uifd/ui-for-docker
+
 docker inspect dockerui | grep -q "\"Running\": true" && {
   echo Stopping running container...
   docker stop dockerui
 }
 
 echo Checking for new version...
-docker pull dockerui/dockerui | grep -q "Downloaded newer image" && {
+docker pull $IMAGE | grep -q "Downloaded newer image" && {
   echo New image version. Removing container...
   docker rm dockerui
 }
 
 echo Starting dockerui...
-docker start dockerui || docker run --name dockerui -d -v /var/run/docker.sock:/var/run/docker.sock -p 9000:9000 dockerui/dockerui
+docker start dockerui || docker run --name dockerui -d -v /var/run/docker.sock:/var/run/docker.sock -p 9000:9000 $IMAGE
